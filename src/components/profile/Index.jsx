7417ae0370ad46530c966af110093ab80b3ad2dc
dongwoon.kim <dongwoon.kim@nts-corp.com>
2022-09-29 18:21:08 +0900

    프로필 사진 임시 추가

--- src/components/profile/Index.jsx
+++ src/components/profile/Index.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 function Index(props) {
 	return (
-		<div>
-			프로필
+		<div className='section section_profile'>
+			<h2 className='blind'>프로필</h2>
+			<img width={200} height={280} src={process.env.PUBLIC_URL + '/assets/img/img-profile.jpg'} alt="프로필 이미지" />
+
 		</div>
 	);
 }
